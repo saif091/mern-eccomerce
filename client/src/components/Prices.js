47361853d2d3c9505b126b2994bc3e6d9a977978
saif <saifmalik7979@gmail.com>
2023-08-24 18:19:00 +0530

    mern-project

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "0rs to 190",
+    array: [0, 190],
+  },
+  {
+    _id: 1,
+    name: "200rs to 390",
+    array: [200, 390],
+  },
+  {
+    _id: 2,
+    name: "400rs to 590",
+    array: [400, 590],
+  },
+  {
+    _id: 3,
+    name: "600rs to 790",
+    array: [600, 790],
+  },
+  {
+    _id: 4,
+    name: "800rs to 990",
+    array: [800, 990],
+  },
+  {
+    _id: 4,
+    name: "1000rs or more",
+    array: [1000, 9999],
+  },
+];
